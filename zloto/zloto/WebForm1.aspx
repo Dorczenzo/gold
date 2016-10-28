fb13c519dd3d8dcbae8f71d85594cbb54b35c782
Dorczenzo <dorian.loewe@gmail.com>
2016-10-28 14:29:03 +0200

    Add files
    Files added

---
+++ zloto/zloto/WebForm1.aspx
@@ -0,0 +1,126 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="zloto.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Ceny złota</title>
+    <%--<link href="styles/jquery-ui-1.8.19.custom.css" rel="stylesheet" />--%>
+    <link href="styles/jquery-ui.css" rel="stylesheet" />
+    <link href="styles/Site.css" rel="stylesheet" />
+    <script src="scripts/jquery-1.7.2.min.js"></script>
+    <script src="scripts/jquery-ui-1.8.19.custom.min.js"></script>
+    <script src="scripts/formatDate.js"></script>
+    <style type="text/css">
+        .auto-style1 {
+            width: 203px;
+            height: 33px;
+        }
+        .auto-style2 {
+            width: 203px;
+            height: 45px;
+        }
+        .auto-style3 {
+            height: 45px;
+        }
+        .auto-style4 {
+            width: 203px;
+            height: 42px;
+        }
+        .auto-style5 {
+            height: 42px;
+        }
+        .auto-style7 {
+            width: 91%;
+            margin-left: 40px;
+        }
+        .auto-style8 {
+            margin-left: 40px;
+        }
+        .auto-style9 {
+            width: 90%;
+            margin-left: 43px;
+        }
+        .auto-style10 {
+            height: 19px;
+        }
+        .auto-style12 {
+            height: 33px;
+        }
+    </style>
+    </head>
+<body>
+    <header>.</header>
+    <img style="width: 980px; height: 100px; max-height: none; max-width: none; display: block; margin: 0 auto;" alt="logo" src="images/gold_header.jpg" />
+    <form class="page" id="form1" runat="server">
+    <h1>Porównywarka cen złota</h1>
+        <p>&nbsp;</p>
+        <div>
+                <table class="auto-style7">
+
+                    <tr>
+                        <td class="auto-style4">
+                            Data początkowa:<asp:TextBox ID="txtStart" runat="server" MaxLength="10">2016-09-29</asp:TextBox>
+                        </td>
+                        <td class="auto-style5">
+                            Dolny zakres cen:<br />
+                            <asp:TextBox ID="priceMinBox" runat="server" MaxLength="6"></asp:TextBox>
+                        </td>
+                    </tr>
+                         <tr>
+                        <td class="auto-style2">
+                            Data końcowa:<asp:TextBox ID="txtEnd" runat="server" MaxLength="10">2016-10-03</asp:TextBox>
+                             </td>
+                        <td class="auto-style3">
+                            Górny zakres cen:<br />
+                            <asp:TextBox ID="priceMaxBox" runat="server" MaxLength="6"></asp:TextBox>
+                             </td>
+                    </tr>
+                         <tr>
+                        <td class="auto-style1">
+            <asp:Button ID="getButton" runat="server" Text="Pobierz dane" />
+                             </td>
+                        <td class="auto-style12">
+                            <asp:Button ID="getFilter" runat="server" Text="Filtruj" Width="62px" />
+                             <asp:Button ID="resetFilter" runat="server" Text="Resetuj" Width="62px"/>
+                             </td>
+                    </tr>
+                </table>
+            <br />
+            <div style="width: 900px; margin-left:auto; margin-right:auto; color: red; text-align: center">
+                <asp:Label ID="errorLabel" runat="server" Text=""></asp:Label>
+            </div>
+            <br />
+
+            <asp:GridView ID="goldTable" runat="server" AllowSorting="True" BackColor="White" BorderColor="#007870" BorderStyle="Solid" BorderWidth="2px" EmptyDataText="------" ForeColor="Black" OnSorting="goldTable_Sorting" Width="900px" CssClass="auto-style8" OnDataBound="goldTable_DataBound" CellPadding="4" HorizontalAlign="Center">
+                <HeaderStyle BackColor="#006666" ForeColor="White" Height="30px" HorizontalAlign="Center" VerticalAlign="Middle" Width="450px" Font-Size="14px" />
+                <RowStyle HorizontalAlign="Center" VerticalAlign="Middle" Width="450px"/>
+            </asp:GridView>
+
+
+                <br />
+                <table class="auto-style9">
+                    <tr>
+                        <td class="auto-style10">
+
+
+            <asp:Label ID="minPriceLabel" runat="server" Text="Label">Najniższa cena (163,32) w dniu 2016/13/43.</asp:Label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class="auto-style10">
+                            <asp:Label ID="maxPriceLabel" runat="server" Text="Label">Najwyższa cena (163,32) w dniu 2016/13/43.</asp:Label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <asp:Label ID="medianLabel" runat="server" Text="Label">Mediana z danego okresu wynosi 163,32.</asp:Label>
+                        </td>
+                    </tr>
+                </table>
+            <p>&nbsp;</p>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ zloto/zloto/WebForm1.aspx.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Xml;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Collections.Generic;
+
+namespace zloto
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string link;
+        private DataTable goldDataTable;
+        Image sortImage = new Image();
+        private string _sortDirection;
+        string startD;
+        string endD;
+        decimal priceMin = 100000;
+        decimal priceMax = 0;
+        decimal startPriceMin;
+        decimal startPriceMax;
+        DateTime dateMin;
+        DateTime dateMax;
+        List<decimal> priceList;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            priceList = new List<decimal>();
+
+            if (!IsPostBack)
+            {
+                txtStart.Text = DateTime.Today.AddDays(-7).ToString("yyyy-MM-dd");
+                txtEnd.Text = DateTime.Today.ToString("yyyy-MM-dd");
+            }
+
+            getButton.Click += new EventHandler(this.GetBtn_Click);
+            getFilter.Click += new EventHandler(this.FltrBtn_Click);
+            resetFilter.Click += new EventHandler(this.RstBtn_Click);
+            Table_Get(txtStart.Text, txtEnd.Text);
+
+        }
+
+        void GetBtn_Click(Object sender, EventArgs e)
+        {
+            startD = txtStart.Text;
+            endD = txtEnd.Text;
+
+            DateTime temp;
+            DateTime temp2;
+            if (DateTime.TryParse(startD, out temp) && DateTime.TryParse(endD, out temp2))
+            {
+
+                if (Convert.ToDateTime(startD) > Convert.ToDateTime(endD))
+                {
+                    errorLabel.Text = "Data początkowa nie może być późniejsza od końcowej.";
+                }
+                else if ((Convert.ToDateTime(endD) - Convert.ToDateTime(startD)).TotalDays > 93)
+                {
+                    errorLabel.Text = "Przekroczono limit serwera. Liczba dni między datami może wynosić maksymalnie 93 dni. Danie nie będą wyświetlane poprawnie!";
+                }
+                else
+                {
+                    errorLabel.Text = null;
+                    Table_Get(startD.ToString(), endD.ToString());
+                    priceMinBox.Text = priceMin.ToString();
+                    priceMaxBox.Text = priceMax.ToString();
+                }
+            }
+            else
+            {
+                errorLabel.Text = "Nieprawidłowy format daty lub data jest wcześniejsza niż rok 2013.";
+            }
+        }
+
+        void FltrBtn_Click(Object sender, EventArgs e)
+        {
+            Filter();
+        }
+
+        void RstBtn_Click(Object sender, EventArgs e)
+        {
+            priceMinBox.Text = startPriceMin.ToString();
+            priceMaxBox.Text = startPriceMax.ToString();
+        }
+
+        void Table_Get(string startDate, string endDate)
+        {
+            link = ("http://api.nbp.pl/api/cenyzlota/" + startDate + "/" + endDate + "?format=xml");
+
+            goldDataTable = new DataTable("GoldPrice");
+
+            for (int i = 0; i < 2; i++)
+            {
+                string cName;
+
+                if (i == 0)
+                {
+                    cName = "Data publikacji";
+                }
+                else
+                {
+                    cName = "Cena 1 g złota (w próbie 1000)";
+                }
+
+                goldDataTable.Columns.Add(cName);
+            }
+
+            XmlDocument doc = new XmlDocument();
+
+            try
+            {
+                doc.Load(link);
+
+                XmlElement root = doc.DocumentElement;
+                XmlNodeList nodes = root.SelectNodes(" / ArrayOfCenaZlota/CenaZlota");
+
+                foreach (XmlNode node in nodes)
+                {
+                    string goldDate = node["Data"].InnerText;
+                    string goldPrice = node["Cena"].InnerText.Replace('.', ',');
+                    priceList.Add(Convert.ToDecimal(goldPrice));
+
+                    string[] obj = new string[2];
+                    obj[0] = goldDate;
+                    obj[1] = goldPrice;
+                    goldDataTable.Rows.Add(obj);
+
+                    if (Convert.ToDecimal(goldPrice) < priceMin)
+                    {
+                        priceMin = Convert.ToDecimal(goldPrice);
+                        dateMin = Convert.ToDateTime(goldDate);
+                    }
+
+                    if (Convert.ToDecimal(goldPrice) > priceMax)
+                    {
+                        priceMax = Convert.ToDecimal(goldPrice);
+                        dateMax = Convert.ToDateTime(goldDate);
+                    }
+                }
+
+                if (!IsPostBack)
+                {
+                    priceMinBox.Text = priceMin.ToString();
+                    priceMaxBox.Text = priceMax.ToString();
+                }
+
+
+                startPriceMin = priceMin;
+                startPriceMax = priceMax;
+
+                minPriceLabel.Text = "Najniższa cena (" + priceMin + ") w dniu " + dateMin.Day + "." + dateMin.Month + "." + dateMin.Year + ".";
+                maxPriceLabel.Text = "Najwyższa cena (" + priceMax + ") w dniu " + dateMax.Day + "." + dateMax.Month + "." + dateMax.Year + ".";
+                medianLabel.Text = "Mediana z danego okresu wynosi " + Median(priceList) + ".";
+
+                BindGrid();
+            }
+            catch
+            {
+                errorLabel.Text = "Błąd w pobieraniu danych z serweru banku. Serwer niedostępny bądź podany zakres obejmuje jedynie dni bez publikacji cen.";
+            }
+            
+        }
+
+
+        private void BindGrid()
+        {
+            goldTable.DataSource = goldDataTable;
+            goldTable.DataBind();
+        }
+
+
+        private decimal Median(List<decimal> list)
+        {
+            list.Sort();
+            int count = list.Count;
+
+            if (count % 2 == 0)
+            {
+                decimal a = list[count / 2 - 1];
+                decimal b = list[count / 2];
+                return Math.Round((decimal)((a + b) / 2), 2);
+            }
+            else
+            {
+                return list[count / 2];
+            }
+
+        }
+
+        void Filter()
+        {
+            if (priceMinBox.Text == "")
+            {
+                priceMinBox.Text = startPriceMin.ToString();
+            }
+
+            if (priceMaxBox.Text == "")
+            {
+                priceMaxBox.Text = startPriceMax.ToString();
+            }
+
+            decimal dTemp;
+            decimal dTemp2;
+            if (decimal.TryParse(priceMinBox.Text, out dTemp) && decimal.TryParse(priceMaxBox.Text, out dTemp2))
+            {
+                errorLabel.Text = "";
+                if (priceMinBox.Text != "" && priceMaxBox.Text != "")
+                {
+                    int numb = goldTable.Rows.Count;
+
+                    for (int i = 0; i < numb; i++)
+                    {
+                        if (Convert.ToDecimal(goldTable.Rows[i].Cells[1].Text) > Convert.ToDecimal(priceMaxBox.Text)
+                            || Convert.ToDecimal(goldTable.Rows[i].Cells[1].Text) < Convert.ToDecimal(priceMinBox.Text))
+                        {
+                            goldTable.Rows[i].Visible = false;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                errorLabel.Text = "Zły format ceny.";
+            }
+        }
+
+        public string SortDirection
+        {
+            get
+            {
+                if (ViewState["SortDirection"] == null)
+                    return string.Empty;
+                else
+                    return ViewState["SortDirection"].ToString();
+            }
+            set
+            {
+                ViewState["SortDirection"] = value;
+            }
+        }
+
+        protected void goldTable_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            SetSortDirection(SortDirection);
+            if (goldDataTable != null)
+            {
+                goldDataTable.DefaultView.Sort = e.SortExpression + " " + _sortDirection;
+                goldTable.DataSource = goldDataTable;
+                goldTable.DataBind();
+                SortDirection = _sortDirection;
+            }
+
+            BindGrid();
+            Filter();
+        }
+
+        protected void SetSortDirection(string sortDirection)
+        {
+            if (sortDirection == "ASC")
+            {
+                _sortDirection = "DESC";
+            }
+            else
+            {
+                _sortDirection = "ASC";
+            }
+        }
+
+        protected void goldTable_DataBound(object sender, EventArgs e)
+        {
+        }
+
+        protected void goldTable_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+        }
+
+        protected void goldTable_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+    }
+}
